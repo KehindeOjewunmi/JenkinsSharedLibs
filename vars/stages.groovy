81de110053e6eaa77895c44280d22ec91b1be6e0
KehindeOjewunmi <120139039+KehindeOjewunmi@users.noreply.github.com>
2023-01-18 23:36:37 -0700

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -2,14 +2,17 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
+       sh "echo code is being built"
        sh "mvn clean package"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
+       sh "echo codequality is being checked"
        sh "mvn clean sonar:sonar"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
+       sh "echo performing artifact back up to nexus"
        sh "mvn clean deploy"
      }
 }
